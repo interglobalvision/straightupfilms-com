45e8020d73a480a758383ecc58f3579de6c4d0a0
Patrick Best <patrickbest@patrickbest.com>
2015-10-12 14:11:14 -0500

    remove obsolete analytics placeholder

--- footer.php
+++ footer.php
@@ -42,8 +42,5 @@
 <script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/jquery-1.9.1.min.js"><\/script>')</script>
 <?php wp_footer(); ?>
 
-<!-- asynchronous google analytics: mathiasbynens.be/notes/async-analytics-snippet change the UA-XXXXX-X to be your site's ID -->
-<script>var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'))</script>
-
 </body>
 </html>
